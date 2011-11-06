b5633c4e8cd4a539dfb019507a49d31d3de01655
Calle Laakkonen <calle@luolamies.org>
2011-11-06 23:22:46 +0200

    Added licensing info.

--- tagquery.h
+++ tagquery.h
@@ -1,3 +1,19 @@
+//
+// This file is part of Piqs.
+// 
+// Piqs is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// Piqs is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with Piqs.  If not, see <http://www.gnu.org/licenses/>.
+//
 #ifndef TAGQUERY_H
 #define TAGQUERY_H
 
